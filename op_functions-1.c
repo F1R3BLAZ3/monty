071b1b4cdb924b2b72161a0e1ab9fe3c2967313f
CKola <christopherkola@gmail.com>
2023-06-22 10:54:04 +0000

    fix to my pop op_functions

--- op_functions-1.c
+++ op_functions-1.c
@@ -147,10 +147,11 @@ void pint(stack_t **stack, unsigned int line_number)
  *
  * Return: This function does not return a value.
  */
+
 void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = *stack;
-	
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
