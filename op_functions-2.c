14f7b56870625c2bf65fdf33c6f93116bda7c0f7
CKola <christopherkola@gmail.com>
2023-06-22 13:28:29 +0000

    I

--- op_functions-2.c
+++ op_functions-2.c
@@ -32,3 +32,37 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	arguments->head = second;
 }
+
+/**
+ * sub - Subtract the top element of the stack from the second top element
+ *
+ * Description: This function subtracts the top element of the stack from
+ * the second top element. If the stack contains less than two elements, it
+ * prints an error message indicating that the stack is too short, followed
+ * by a new line, and exits with a failure status code.
+ * The result of the subtraction is stored in the second top element of the
+ * stack, and the top element is removed, making the stack one element shorter.
+ * At the end, the top element of the stack contains the result.
+ *
+ * @stack: Double pointer to the head of the stack
+ * @line_number: The line number being executed in the Monty file
+ *
+ * Return: This function does not return a value.
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int result;
+	(void)stack;
+
+	if (arguments->head == NULL || arguments->head->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	result = arguments->head->next->n - arguments->head->n;
+
+	pop(&arguments->head, line_number);
+	arguments->head->n = result;
+}
